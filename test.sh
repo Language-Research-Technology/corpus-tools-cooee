7d44803d0a9e1270bd13f62714a995378d715f46
Alvin Sebastian <a.sebastian@connect.qut.edu.au>
2025-05-23 11:28:43 +1000

    Update oni-ocfl version

--- test.sh
+++ test.sh
@@ -15,6 +15,6 @@ TEMP_DIR=${BASE_TEMP_DIR}
 node index.js -r "${REPO_OUT_DIR}" \
 	-t "${BASE_DATA_DIR}" -n ${REPO_NAME} \
 	-s ${NAMESPACE} -x "${BASE_DATA_DIR}"/COOEE.XLS \
-	-z "${REPO_SCRATCH_DIR}" 
-#	-vx -vm "./node_modules/ro-crate-modes/modes/comprehensive-ldac.json"
+	-z "${REPO_SCRATCH_DIR}"
+# 	--vx --vm "./node_modules/ro-crate-modes/modes/comprehensive-ldac.json"
 #	-p "${TEMP_DIR}" \
