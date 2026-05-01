ff4c73d04d0bcc17248c7bf512168c18babca797
Alvin Sebastian <a.sebastian@connect.qut.edu.au>
2026-05-01 04:29:20 +0000

    Update ocfl. Add metadata license.

--- prod.sh
+++ prod.sh
@@ -6,7 +6,7 @@ REPO_OUT_DIR=/opt/storage/oni/ocfl
 
 BASE_DATA_DIR=./cooee-attachments
 REPO_NAME=LDaCA
-NAMESPACE=corpus-of-oz-early-english
+#NAMESPACE=corpus-of-oz-early-english
 NAMESPACE=hdl10.26180~23961609
 
 node index.js -r "${REPO_OUT_DIR}" \
